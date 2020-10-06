160b2438d1884f1e6df83d51669bf45c0a173f7d
Maranel <50551100+Maranel@users.noreply.github.com>
2020-10-06 21:26:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,10 @@
 //   });
 
   $("#bt1").click(() => {
-    // it's the same as before
     console.log("Yeah, you clicked me")
   })
   $(".bt2").click(() => {
-    // it's the same as before
+
     $("#bt1").text("I was changed")
   })
   $(".bt3").click(() =>{
